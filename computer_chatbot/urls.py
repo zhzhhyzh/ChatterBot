b5dfad637fd834353c66aefb4f2a738af8f4c6e7
quintonpyx <quinton@33digitec.com>
2024-09-02 21:08:26 +0800

    change url

--- computer_chatbot/urls.py
+++ computer_chatbot/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chatbot/', include('chatbot.urls')),
+    path('', include('chatbot.urls')),
 ]
